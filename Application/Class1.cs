5097f0777a1543adada540e1edda26b4907b09be
sigurtO <sigurt.olsen@hotmail.com>
2025-09-24 11:20:57 +0200

    Add project files.

---
+++ Application/Class1.cs
@@ -0,0 +1,18 @@
+﻿using Application;
+
+namespace Application
+{
+    public class Booking
+    {
+
+        public Guid BookingId { get; set; } //Globally Unique Identifie
+        public Guid GuestId { get; set; }
+        public Guid ApartmentId { get; set; }
+        public DateTime StayStart { get; set; }
+        public DateTime StayEnd { get; set; }
+        public int Price { get; set; }
+        public bool Payment { get; set; }
+        public bool Booked { get; set; }
+    }
+}
+
